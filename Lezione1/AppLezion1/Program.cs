734884142a391021135314c6efb85b78a382b6d7
raulubaldi01 <raulubaldi01@outlook.it>
2018-09-16 11:02:06 +0200

    Lezione1

---
+++ Lezione1/AppLezion1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLezion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 55555;
+            Console.WriteLine($"il vaore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"il vaore di s è {s}");
+        }
+    }
+}
